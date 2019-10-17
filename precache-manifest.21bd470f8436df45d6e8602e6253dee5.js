82dbda0b5335e1ae50e6c5991d6858a5189c90d6
Nadia Iqbal <nadee.shocks@gmail.com>
2019-10-18 00:01:44 +0500

    Updates

---
+++ precache-manifest.21bd470f8436df45d6e8602e6253dee5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b287fe786b8d3be64495baeb1c747c",
+    "url": "/Nadeeshocks.github.io/hochburg/index.html"
+  },
+  {
+    "revision": "97af39c0c6b762b5f593",
+    "url": "/Nadeeshocks.github.io/hochburg/static/css/main.2a2c715d.chunk.css"
+  },
+  {
+    "revision": "a609d636b284f3b708e1",
+    "url": "/Nadeeshocks.github.io/hochburg/static/js/2.577c44fc.chunk.js"
+  },
+  {
+    "revision": "97af39c0c6b762b5f593",
+    "url": "/Nadeeshocks.github.io/hochburg/static/js/main.7e50c573.chunk.js"
+  },
+  {
+    "revision": "af5e8f7f5a6f787f3192",
+    "url": "/Nadeeshocks.github.io/hochburg/static/js/runtime-main.5ab0d678.js"
+  }
+]);
